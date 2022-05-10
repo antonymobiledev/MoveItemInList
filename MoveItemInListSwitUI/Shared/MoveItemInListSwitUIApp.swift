89112ef1fe9f9b514cd6661f791bc08db8ec35c0
Antony <antonyouseph@gmail.com>
2022-05-10 10:25:03 +0530

    Initial Commit
    Project set up commit

---
+++ MoveItemInListSwitUI/Shared/MoveItemInListSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveItemInListSwitUIApp.swift
+//  Shared
+//
+//  Created by Antony K O on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MoveItemInListSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
